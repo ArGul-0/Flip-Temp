a8ed9b0828f1f162f9a899aa5e5da7f05c5af0a3
ArGul-0 <argul.mail@gmail.com>
2025-09-03 23:57:52 +0300

    Добавьте файлы проекта.

---
+++ Flip Temp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Flip_Temp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
